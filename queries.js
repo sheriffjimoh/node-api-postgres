a1c3660bda935876e7e86fd81c3ca0e14fb21af9
sheriffjimoh <sheriffjimoh88@gmail.com>
2022-01-31 22:42:34 -0800

    initial commit

---
+++ queries.js
@@ -0,0 +1,84 @@
+let {pool: pool} = require('./connection');
+
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  
+  const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  
+  const createUser = (request, response) => {
+    const { name, email } = request.body;
+    var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+  const isvalid =  emailRegex.test(email);
+    if( name == undefined){
+      response.status(400).json({message:"name can't be empty"})
+      return;
+    }
+    if( email == undefined){
+      response.status(400).json({message:"email can't be empty"})
+      return;
+    }
+
+    if(!isvalid){
+      response.status(400).json({message:"invalid email address"})
+      return;
+    }
+    
+    pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({message:`User added successfully`})
+    })
+
+  }
+  
+  const updateUser = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { name, email } = request.body
+  
+    pool.query(
+      'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+      [name, email, id],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`User modified with ID: ${id}`)
+      }
+    )
+  }
+  
+  const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User deleted with ID: ${id}`)
+    })
+  }
+  
+  module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+  }
